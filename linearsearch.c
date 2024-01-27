10bb15717b81e9986267d7cd49b37c253967bcd7
Arjun <acharyaarjun410@gmail.com>
2024-01-27 12:49:16 +0530

    linear search updated

--- linearsearch.c
+++ linearsearch.c
@@ -1,1 +1,33 @@
+#include<stdio.h>
+#include<stdlib.h>
 
+int main()
+{
+    int key,a[30],i,n;
+
+    printf("Enter the number of elements");
+    scanf("%d",&n);
+    printf("Enter the array elements");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("Enter the key element");
+    scanf("%d",&key);
+    
+    for(i=0;i<n;i++)
+    {
+        if(a[i]==key)
+       { printf("Elemennt %d found at position %d",key,i);
+        exit(0);
+       }
+    }
+    
+    if (i==n)
+    {
+        printf("Element not found");
+    }
+    
+    return 0;
+
+}
